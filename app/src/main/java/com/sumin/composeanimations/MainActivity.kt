a9ab424fb2c08a31ca50b572ba0dfeba00868fdf
Nejer <nejer6@gmail.com>
2024-02-25 09:08:33 +0300

    9.8 Анимации часть 1. AnimateAsState

--- app/src/main/java/com/sumin/composeanimations/MainActivity.kt
+++ app/src/main/java/com/sumin/composeanimations/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.sumin.composeanimations.ui.screen.Test
 import com.sumin.composeanimations.ui.theme.ComposeAnimationsTheme
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
             ComposeAnimationsTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     Test()
                 }
